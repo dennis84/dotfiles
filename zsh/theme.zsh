8e8d55285205eef9bd77aa9cfcd4ce7ba9464a85
Dennis Dietrich <d.dietrich84@googlemail.com>
2012-12-16 19:12:02 +0100

    updated zsh prompt

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -2,12 +2,13 @@ ZSH_THEME_GIT_PROMPT_PREFIX=" %{$reset_color%}±(%{$fg[green]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%})"
 MODE_INDICATOR="%{$fg[yellow]%}<VI>%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_SHA_BEFORE="%{$reset_color%}%{$fg[cyan]%} "
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_bold[red]%}✗"
 
 my_git_prompt_info() {
   [ ! -f .git/HEAD ] && return
   ref=$(git symbolic-ref HEAD 2> /dev/null) || ref="*detached*"
   if [ -f .git/BISECT_START ]; then bisect="*bisect*"; else bisect=""; fi
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_short_sha)$bisect$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_short_sha)$bisect$ZSH_THEME_GIT_PROMPT_SUFFIX $(parse_git_dirty)"
 }
 
 returncode="%(?.%{$fg[yellow]%} λ .%{$fg[red]%} λ )"
