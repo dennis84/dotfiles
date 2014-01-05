c83b94cf3f43c0fbbac3fbb6607303ebf55bf333
dennis84 <d.dietrich84@gmail.com>
2014-01-05 15:23:20 +0100

    some updates

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,10 @@ set -x PATH /sbin /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin $PATH
 set -x PATH "$HOME/bin" "$HOME/dotfiles/scripts" $PATH
 set -x PATH "$HOME/code/play" $PATH
 set -x PATH "$HOME/.gem/ruby/2.0.0/bin" $PATH
+set -x PATH "/root/.gem/ruby/2.0.0/bin" $PATH
 set -x PATH "$HOME/.cabal/bin" $PATH
+set -x PATH "/usr/local/heroku/bin" $PATH
+
 
 set -x GEM_HOME "$HOME/.gem/ruby/2.0.0"
 
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -12,16 +12,12 @@ import qualified XMonad.StackSet as W
 main = xmonad $ withUrgencyHook dzenUrgencyHook { args = ["-bg", "darkgreen", "-xs", "1"] }
               $ myConfig
 
-------------------------------------------------------------------------
--- Workspaces
--- The default number of workspaces (virtual screens) and their names.
---
 myWorkspaces = ["1:system","2:main","3:network"] ++ map show [4..6]
 
 myConfig = defaultConfig {
       terminal   = "urxvtc -e ~/.tmux/menu"
     , modMask    = mod1Mask -- Alt
-    , layoutHook = smartBorders $ noBorders Full
+    , layoutHook = smartBorders $ layoutHook defaultConfig
     , workspaces = myWorkspaces
   }
   `additionalKeysP` [
