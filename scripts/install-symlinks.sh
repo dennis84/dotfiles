c2cdedf425767d63e1e2191466786d2ccf898b29
Dennis Dietrich <d.dietrich84@gmail.com>
2016-07-12 09:44:56 +0200

    osx stuff

--- scripts/install-symlinks.sh
+++ scripts/install-symlinks.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Absolute path to this script
-SCRIPT=`readlink -f $0`
 # Absolute path this script is in
-SCRIPTPATH=`dirname $SCRIPT`
+SCRIPTPATH="$(dirname "$0")"
 
 echo "Linking config files from $SCRIPTPATH"
 
