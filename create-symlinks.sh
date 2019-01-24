e2a09f5ed990b1c8f06cffda86d6f7a955e70f82
Dennis Dietrich <d.dietrich84@gmail.com>
2019-01-24 23:24:39 +0100

    stuff

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p ~/.config
+
 ln -s ~/dotfiles/xinitrc ~/.xinitrc
 ln -s ~/dotfiles/i3 ~/.config/i3
 ln -s ~/dotfiles/i3status.conf ~/.i3status.conf
@@ -7,10 +9,9 @@ ln -s ~/dotfiles/tmux ~/.tmux
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore ~/.gitignore_global
 ln -s ~/dotfiles/agignore ~/.agignore
-ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
-ln -s ~/dotfiles/fish/functions ~/.config/fish/functions
+ln -s ~/dotfiles/fish ~/.config/fish
 ln -s ~/dotfiles/ctags ~/.ctags
-ln -s ~/dotfiles/alacritty.yml ~/.alacritty.yml
+ln -s ~/dotfiles/alacritty ~/.config/alacritty
 
 unamestr="$(uname)"
 if [[ "$unamestr" == 'Darwin' ]]; then
