4316917f7c4e34430730af4f26e2b893e6452b9d
Dietrich, Dennis [CBC-Extern] <d.dietrich84@gmail.com>
2024-04-24 09:37:46 +0200

    switch color_scheme on system darkmode

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -39,4 +39,24 @@ config.keys = {
 	},
 }
 
+-- switch theme on system darkmode
+
+local function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'tokyonight_moon'
+  else
+    return 'tokyonight_day'
+  end
+end
+
+wezterm.on('window-config-reloaded', function(window)
+  local overrides = window:get_config_overrides() or {}
+  local appearance = window:get_appearance()
+  local scheme = scheme_for_appearance(appearance)
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
+  end
+end)
+
 return config
